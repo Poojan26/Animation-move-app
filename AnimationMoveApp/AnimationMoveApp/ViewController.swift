587db9b75113a4103ed364b7724f13c4640f9dcd
Poojan Patel <poojan.patel1126@gmail.com>
2022-03-19 06:32:00 -0700

    Added damping and velocity

--- AnimationMoveApp/AnimationMoveApp/ViewController.swift
+++ AnimationMoveApp/AnimationMoveApp/ViewController.swift
@@ -17,19 +17,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        UIView.animate(withDuration: 2.0) {
-        self.myLabel.center.x += self.view.bounds.width
-        self.myTextField.center.x += self.view.bounds.width
-        self.myImageView.center.x += self.view.bounds.width
-        }
+        //UIView.animate(withDuration: 2.0) {
+        //self.myLabel.center.x += self.view.bounds.width
+        //self.myTextField.center.x += self.view.bounds.width
+        //self.myImageView.center.x += self.view.bounds.width
+        
+        // Velocity and damping
+        UIView.animate(withDuration: 2.0, delay: 0.5, usingSpringWithDamping: 0.1, initialSpringVelocity: 0.5, options: [.repeat, .autoreverse], animations: {
+            self.myLabel.center.x += self.view.bounds.width
+            self.myTextField.center.x += self.view.bounds.width
+            self.myImageView.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+    }
         
     }
     
     // Appear from left
-    override func viewWillAppear(_ animated: Bool) {
-        myLabel.center.x -= view.bounds.width
-        myTextField.center.x -= view.bounds.width
-        myImageView.center.x -= view.bounds.width
-    }
-}
-
+    //override func viewWillAppear(_ animated: Bool) {
+    //    myLabel.center.x -= view.bounds.width
+    //    myTextField.center.x -= view.bounds.width
+    //    myImageView.center.x -= view.bounds.width
+   // }
+    
+    
